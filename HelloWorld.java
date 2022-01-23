e5efb7ffe50b79a3bdb30b2004989a79fa168113
Matthew Schildknecht <coolphysics@me.com>
2022-01-23 11:39:33 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * HelloWorld.java
+ * A first Java program
+ * @author Mr Schildknecht
+ * @version 2022-01-17
+ */
+
+import java.util.*;
+
+public class HelloWorld
+{
+    public static void main(String[] args) 
+    {
+        //Displays a greeting
+       System.out.print("Hello World!"); //Hello Jayden
+       System.out.print("Goodbye, ");
+       System.out.println("cruel world");
+       System.out.print("Hello! How are you doing?");
+       System.out.println("She said \"Hello!\" to me.");
+       System.out.println("She said \tHello! to me.");
+       System.out.println("She said \\Hello!\" to me.");
+
+    }
+}
